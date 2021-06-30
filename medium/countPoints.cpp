be82f7eea70d7b32b5d09024bde2427166283398
Anurag Pal <apal895@gmail.com>
2021-06-30 22:20:38 +0530

    leetcode med started

---
+++ medium/countPoints.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+int euclidianDistance(int x1, int y1, int x2, int y2)
+{
+    return ceil(sqrt(pow((x1 - x2), 2) + pow(y1 - y2, 2)));
+}
+
+vector<int> countPoints(vector<vector<int>> &points, vector<vector<int>> &queries)
+{
+    // attempting brute force
+    vector<int> res;
+    for (vector<int> query : queries)
+    {
+        int count = 0;
+        for (vector<int> point : points)
+        {
+            if (euclidianDistance(query[0], query[1, point[0], point[1]) <= query[2])
+            {
+                count++;
+            }
+        }
+        res.push_back(count);
+    }
+    return res;
+}
+
+int main()
+{
+    // do something
+    return 0;
+}
